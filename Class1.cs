d7849a5a6d7f7bb9e41012ad97bb9e3e4de15fb0
fineracoon <66884461+fineracoon@users.noreply.github.com>
2020-06-23 11:14:09 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task6
+{
+    public class Employee : IComparable<Employee>
+    {
+        private long empID;
+        private string empName;
+        private DateTimeOffset empBirthday;
+        private double empSalary;
+
+        public Employee()
+        {
+            empID = 0;
+            empName = "";
+            empBirthday = DateTimeOffset.MinValue;
+            empSalary = 0.0;
+        }
+
+        public Employee(long id, string name, DateTimeOffset birthday, double salary)
+        {
+            empID = id;
+            empName = name;
+            empBirthday = birthday;
+            empSalary = salary;
+        }
+
+        public Employee(Employee employee)
+        {
+            this.empID = employee.empID;
+            this.empName = employee.empName;
+            this.empBirthday = employee.empBirthday;
+            this.empSalary = employee.empSalary;
+        }
+
+        public void SetId(long id)
+        {
+            empID = id;
+        }
+
+        public long GetId()
+        {
+            return empID;
+        }
+
+        public void SetName(string name)
+        {
+            empName = name;
+        }
+
+        public string GetName()
+        {
+            return empName;
+        }
+
+        public void SetBirthday(DateTimeOffset bd)
+        {
+            empBirthday = bd;
+        }
+
+        public DateTimeOffset GetBirthday()
+        {
+            return empBirthday;
+        }
+
+        public void SetSalary(double salary)
+        {
+            empSalary = salary;
+        }
+
+        public double GetSalary()
+        {
+            return empSalary;
+        }
+
+        
+        public void toString()
+        {
+            Console.Write("ID: " + this.empID + "\r\n" +
+                   "Name: " + this.empName + "\r\n" +
+                   "Date of birthday: " + this.empBirthday.ToString("dd.MM.yyyy") + "\r\n" +
+                   "Salary: " + empSalary + "\r\n");
+        }
+
+        public int CompareTo(Employee other)
+        {
+            if (this.GetBirthday() < other.GetBirthday())
+                return 1;
+            else if (this.GetBirthday() > other.GetBirthday())
+                return -1;
+            else return 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task6
+{
+    class Program
+    {
+        static void SortByBirthday(ref List<Employee> employees)
+        {
+            for (int i = 0; i < employees.Count - 1; i++)
+            {
+                for (int j = i + 1; j < employees.Count; j++)
+                {
+                    if (employees[i].GetBirthday().CompareTo(employees[j].GetBirthday()) > 0)
+                    {
+                        Employee buffer = employees[i];
+                        employees[i] = employees[j];
+                        employees[j] = buffer;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee(24847, "John Smith", new DateTimeOffset(new DateTime(1985, 11, 25)), 350.50);
+            Employee e2 = new Employee();
+            e2.SetId(14598);
+            e2.SetName("Adam Sendler");
+            e2.SetBirthday(new DateTimeOffset(new DateTime(1990, 9, 12)));
+            e2.SetSalary(280.70);
+            Employee e3 = new Employee(e1);
+            e3.SetId(35478);
+
+            List<Employee> employees = new List<Employee>();
+            employees.Add(e1);
+            employees.Add(e2);
+            employees.Add(e3);
+
+            Console.WriteLine("Before sorting");
+            foreach (Employee e in employees)
+            {
+                e.toString();
+            }
+
+            SortByBirthday(ref employees);
+
+            Console.WriteLine("After sorting");
+            foreach (Employee e in employees)
+            {
+                e.toString();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
