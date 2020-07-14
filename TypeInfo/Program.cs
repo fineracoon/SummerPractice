08a2e208e70fbbc37e4cc76423b994eb7445b56a
fineracoon <66884461+fineracoon@users.noreply.github.com>
2020-07-14 12:37:25 +0300

    Add files via upload

---
+++ TypeInfo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+namespace TypeInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type myType = Type.GetType("TypeInfo.User", false, true);
+
+            foreach (MemberInfo mi in myType.GetMembers())
+            {
+                Console.WriteLine($"{mi.DeclaringType} {mi.MemberType} {mi.Name}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TypeInfo/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypeInfo
+{
+    class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public User(string n, int a)
+        {
+            Name = n;
+            Age = a;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Имя: {Name}  Возраст: {Age}");
+        }
+        public int Payment(int hours, int perhour)
+        {
+            return hours * perhour;
+        }
+    }
+}
